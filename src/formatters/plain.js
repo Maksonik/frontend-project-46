19708b18868a03a3265758cef9d8c44b99691715
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2024-12-05 11:37:30 +0500

    1

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,18 +2,18 @@ import formatValue from '../formatValue.js';
 
 export default function plain(diff, parent = '') {
   const lines = Object.entries(diff).map(([key, value]) => {
-    const { status, ...rest } = value;
+    const status = value.status;
     const fullKey = parent ? `${parent}.${key}` : key;
 
     switch (status) {
       case 'added':
-        return `Property '${fullKey}' was added with value: ${formatValue(rest.value)}`;
+        return `Property '${fullKey}' was added with value: ${formatValue(value.value)}`;
       case 'removed':
         return `Property '${fullKey}' was removed`;
       case 'changed':
-        return `Property '${fullKey}' was updated. From ${formatValue(rest.oldValue)} to ${formatValue(rest.newValue)}`;
+        return `Property '${fullKey}' was updated. From ${formatValue(value.oldValue)} to ${formatValue(value.newValue)}`;
       case 'nested':
-        return plain(rest.children, fullKey);
+        return plain(value.children, fullKey);
       default:
         return '';
     }

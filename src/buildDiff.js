bf7a19178edec61651b29a5d9536a67f62e66550
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2024-12-05 11:43:55 +0500

    1

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,23 +1,20 @@
 export default function buildDiff(data1, data2) {
   const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])];
-  const sortedKeys = keys.slice().sort();
+  const sortedKeys = Array.from(keys).sort(); // создаем новый массив и сортируем его
   const diff = sortedKeys.reduce((acc, key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
     if (!(key in data1)) {
-      acc[key] = { status: 'added', value: value2 };
-    } else if (!(key in data2)) {
-      acc[key] = { status: 'removed', value: value1 };
-    } else if (value1 !== null && typeof value1 === 'object' && value2 !== null && typeof value2 === 'object') {
-      acc[key] = { status: 'nested', children: buildDiff(value1, value2) };
-    } else if (value1 !== value2) {
-      acc[key] = { status: 'changed', oldValue: value1, newValue: value2 };
-    } else {
-      acc[key] = { status: 'unchanged', value: value1 };
+      return { ...acc, [key]: { status: 'added', value: value2 } };
+    } if (!(key in data2)) {
+      return { ...acc, [key]: { status: 'removed', value: value1 } };
+    } if (value1 !== null && typeof value1 === 'object' && value2 !== null && typeof value2 === 'object') {
+      return { ...acc, [key]: { status: 'nested', children: buildDiff(value1, value2) } };
+    } if (value1 !== value2) {
+      return { ...acc, [key]: { status: 'changed', oldValue: value1, newValue: value2 } };
     }
-
-    return acc;
+    return { ...acc, [key]: { status: 'unchanged', value: value1 } };
   }, {});
   return diff;
 }
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -19,34 +19,34 @@ function formatStylish(diff, depth = 0) {
   const indentSize = 4;
   const currentIndent = ' '.repeat(depth * indentSize);
   const nextIndent = ' '.repeat((depth + 1) * indentSize);
-  const lines = [];
 
-  Object.entries(diff).forEach(([key, value]) => {
-    const { status } = value;
-    const children = status === 'nested' ? formatStylish(value.children, depth + 1) : undefined;
-    switch (status) {
-      case 'added':
-        lines.push(`${currentIndent}  + ${key}: ${formatValue(value.value, depth + 1)}`);
-        break;
-      case 'removed':
-        lines.push(`${currentIndent}  - ${key}: ${formatValue(value.value, depth + 1)}`);
-        break;
-      case 'changed':
-        lines.push(`${currentIndent}  - ${key}: ${formatValue(value.oldValue, depth + 1)}`);
-        lines.push(`${currentIndent}  + ${key}: ${formatValue(value.newValue, depth + 1)}`);
-        break;
-      case 'nested':
-        lines.push(`${currentIndent}    ${key}: {\n${children}\n${nextIndent}}`);
-        break;
-      case 'unchanged':
-        lines.push(`${currentIndent}    ${key}: ${formatValue(value.value, depth + 1)}`);
-        break;
-      default:
-        console.warn(`Unknown status: ${status}`);
-    }
-  });
+  return Object.entries(diff)
+    .map(([key, value]) => {
+      const { status } = value;
+      const children = status === 'nested' ? formatStylish(value.children, depth + 1) : undefined;
 
-  return lines.join('\n');
+      switch (status) {
+        case 'added':
+          return `${currentIndent}  + ${key}: ${formatValue(value.value, depth + 1)}`;
+        case 'removed':
+          return `${currentIndent}  - ${key}: ${formatValue(value.value, depth + 1)}`;
+        case 'changed':
+          return [
+            `${currentIndent}  - ${key}: ${formatValue(value.oldValue, depth + 1)}`,
+            `${currentIndent}  + ${key}: ${formatValue(value.newValue, depth + 1)}`,
+          ];
+        case 'nested':
+          return `${currentIndent}    ${key}: {\n${children}\n${nextIndent}}`;
+        case 'unchanged':
+          return `${currentIndent}    ${key}: ${formatValue(value.value, depth + 1)}`;
+        default:
+          console.warn(`Unknown status: ${status}`);
+          return '';
+      }
+    })
+    .flat()
+    .filter(Boolean)
+    .join('\n');
 }
 
 export default function stylish(diff) {

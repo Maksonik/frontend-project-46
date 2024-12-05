a81011520fb27a23894d0e80289c19fdc74fa76e
Sustavov Maksim <maksim.sustavov@edgecenter.ru>
2024-12-05 08:49:41 +0500

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,16 +3,21 @@
 import { program } from 'commander';
 import generateDiff from '../src/index.js';
 
-export default generateDiff
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file')
-  .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => {
-    console.log(generateDiff(filepath1, filepath2, options.format));
-  });
+export function createProgram() {
+  const program = commander
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>', 'path to first file')
+    .argument('<filepath2>', 'path to second file')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .action((filepath1, filepath2, options) => {
+      console.log(generateDiff(filepath1, filepath2, options.format));
+    });
 
-program.parse();
+  return program;
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  createProgram().parse();
+}
